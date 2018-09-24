1586764a79d5b936b99e2e90395be72bef397b7b
Yoseph-Tajul-Arifin <yosephtajularifin87@gmail.com>
2018-09-24 19:48:30 +0700

    versi 1.0.0

---
+++ application/controllers/Jhones.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Jhones extends CI_Controller {
+
+	public function beranda()
+	{
+		echo "Hai Jhones";
+	}
+
+	public function on()
+	{
+		echo "Aku Siaap";
+	}
+
+	public function air()
+	{
+		echo "Kalo Haus Minum Pak";
+	}
+}
